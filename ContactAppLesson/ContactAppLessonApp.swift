78a214e377d9742c7a46dd03271f0ce3fa257dcd
Oncu <oohancan@gmail.com>
2022-08-04 12:48:34 +0300

    Initial Commit

---
+++ ContactAppLesson/ContactAppLessonApp.swift
@@ -0,0 +1,17 @@
+//
+//  ContactAppLessonApp.swift
+//  ContactAppLesson
+//
+//  Created by Oncu Can on 4.08.2022.
+//
+
+import SwiftUI
+
+@main
+struct ContactAppLessonApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
